cbf9eb18ce4afa56aa340861006e44246b74f41e
fadhilswl <89720248+fadhilswl@users.noreply.github.com>
2026-06-16 12:19:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = "finance-shuffle-v5"; 
-const ASSETS = [ "./", "./index.html", "./style.css", "./script.js", "./manifest.json" ];
+const CACHE_NAME = "finance-emoji-v7"; 
+const ASSETS = [ 
+  "./", 
+  "./index.html", 
+  "./style.css", 
+  "./script.js", 
+  "./manifest.json" 
+];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", (e) => {
-  if (!e.request.url.includes('script.google.com') && !e.request.url.includes('fonts.googleapis.com') && !e.request.url.includes('fonts.gstatic.com')) {
+  if (!e.request.url.includes('script.google.com') && 
+      !e.request.url.includes('fonts.googleapis.com') && 
+      !e.request.url.includes('fonts.gstatic.com')) {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
   }
 });

361741fbd0c701a0673b860c034cd32fdee1c5b1
fadhilswl <89720248+fadhilswl@users.noreply.github.com>
2026-06-16 11:21:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finance-theme-v3"; // Versi diperbarui untuk memaksa clear cache
+const CACHE_NAME = "finance-rgb-v4"; 
 const ASSETS = [ "./", "./index.html", "./style.css", "./script.js", "./manifest.json" ];
 
 self.addEventListener("install", (e) => {
